af0c4c057cabb48aad5e4932fefb33c2ebb7995b
Максим Фролов <mr.mfrolov@yandex.com>
2026-01-03 17:44:47 +0500

    added app goods with catalog and urls

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
+    'goods',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [ BASE_DIR / 'static']
--- main/views.py
+++ main/views.py
@@ -4,12 +4,14 @@ from django.shortcuts import render
 def index(request):
     context = {
         "title": "Главная",
-        "content": "Контент главной страницы",
-        "list_items": ["first 1", "second 2"],
-        "dict_items": {"One": 1},
-        "is_authenticated": True,
+        "content": "Магазин мебели HOME",
     }
     return render(request, "main/index.html", context)
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "О нас",
+        "content": "О нас",
+        "text_on_page": "Текст на странице о интернет магазине",
+    }
+    return render(request, "main/about.html", context)
