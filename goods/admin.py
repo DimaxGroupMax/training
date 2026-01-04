ec3559d3ee4646118b8715c971f206022b54ae6d
Максим Фролов <mr.mfrolov@yandex.com>
2026-01-04 19:57:05 +0500

    Выведены категории продуктов из базы данных. Выведены продукты из базы данных.

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Categories, Products
 
-# Register your models here.
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description', 'price', 'discount', 'quantity')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    prepopulated_fields = {'slug': ('name',)}
