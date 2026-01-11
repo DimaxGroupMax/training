f11ebc8633878e8ee5a970501adb73638f67ebd8
Максим Фролов <mr.mfrolov@yandex.com>
2026-01-11 12:58:11 +0500

    Построены url маршруты

--- goods/views.py
+++ goods/views.py
@@ -1,21 +1,25 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-
 from goods.models import Products
 
 
-def catalog(request):
-    goods = Products.objects.all()
-    context = {
-        'title': 'Каталог товаров',
-        'goods':  goods
-    }
-    return render(request, 'goods/catalog.html', context=context)
+def catalog(request, category_slug) -> HttpResponse:
+ if category_slug == 'all':
+  goods = Products.objects.all()
+ else:
+  goods = Products.objects.filter(category__slug=category_slug)
+ context = {
+  'title': 'Каталог товаров',
+  'goods': goods
+ }
+ return render(request, 'goods/catalog.html', context=context)
 
-def product(request, product_slug=False)-> HttpResponse:
 
-    product = Products.objects.get(slug=product_slug)
-    context = {
-        "product": product
-    }
-    return render(request, 'goods/product.html', context=context)
+def product(request, product_slug) -> HttpResponse:
+ product = Products.objects.get(slug=product_slug)
+ context = {
+  "product": product
+ }
+ return render(request, 'goods/product.html', context=context)
+
