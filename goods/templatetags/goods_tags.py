5a7261d620ab00aaed94cbfff9929c8f66b13241
Максим Фролов <mr.mfrolov@yandex.com>
2026-01-11 12:58:11 +0500

    Построены url маршруты

---
+++ goods/templatetags/goods_tags.py
@@ -0,0 +1,8 @@
+﻿from goods.models import  Categories
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def tag_categories():
+    return Categories.objects.all()
--- main/views.py
+++ main/views.py
@@ -4,11 +4,10 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
+
     context = {
         "title": "Главная",
         "content": "Магазин мебели HOME",
-        "categories": categories
     }
     return render(request, "main/index.html", context)
 
